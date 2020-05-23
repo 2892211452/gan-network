ea2226287756491d3830746a4648a0a46cc27bbb
2892211452 <2892211452@qq.com>
2020-05-23 21:21:59 +0800

    init

---
+++ Mygan.py
@@ -0,0 +1,105 @@
+"""
+以莫烦python的思路为基础
+Dependencies:
+torch: 0.4
+numpy
+matplotlib
+"""
+import torch
+import torch.nn as nn
+import numpy as np
+import matplotlib.pyplot as plt
+
+# torch.manual_seed(1)    # reproducible
+# np.random.seed(1)
+
+# Hyper Parameters
+BATCH_SIZE = 64
+LR_G = 0.0001           # learning rate for generator
+LR_D = 0.0001           # learning rate for discriminator
+N_IDEAS = 5             # think of this as number of ideas for generating an art work (Generator)
+ART_COMPONENTS = 15     # it could be total point G can draw in the canvas
+PAINT_POINTS = np.vstack([np.linspace(-1, 1, ART_COMPONENTS) for _ in range(BATCH_SIZE)])
+
+# show our beautiful painting range
+# plt.plot(PAINT_POINTS[0], 2 * np.power(PAINT_POINTS[0], 2) + 1, c='#74BCFF', lw=3, label='upper bound')
+# plt.plot(PAINT_POINTS[0], 1 * np.power(PAINT_POINTS[0], 2) + 0, c='#FF9359', lw=3, label='lower bound')
+# plt.legend(loc='upper right')
+# plt.show()
+
+
+def artist_works():     # painting from the famous artist (real target)
+    a = np.random.uniform(1, 2, size=BATCH_SIZE)[:, np.newaxis]
+    paintings = a * np.power(PAINT_POINTS, 2) + (a-1)
+    paintings = torch.from_numpy(paintings).float()
+    return paintings
+
+G = nn.Sequential(                      # Generator
+    nn.Linear(N_IDEAS, 128),            # random ideas (could from normal distribution)
+    nn.ReLU(),
+    nn.Linear(128, ART_COMPONENTS),     # making a painting from these random ideas
+)
+
+D = nn.Sequential(                      # Discriminator
+    nn.Linear(ART_COMPONENTS, 128),     # receive art work either from the famous artist or a newbie like G
+    nn.ReLU(),
+    nn.Linear(128, 1),
+    nn.Sigmoid(),                       # tell the probability that the art work is made by artist
+)
+
+opt_D = torch.optim.Adam(D.parameters(), lr=LR_D)
+opt_G = torch.optim.Adam(G.parameters(), lr=LR_G )
+
+plt.ion()   # something about continuous plotting
+criterion = nn.BCELoss() # 二值交叉熵 Binary cross entropy loss
+
+for step in range(10000):
+    artist_paintings = artist_works()           # real painting from artist
+    G_ideas = torch.randn(BATCH_SIZE, N_IDEAS)  # random ideas
+    G_paintings = G(G_ideas)                    # fake painting from G (random ideas)
+
+    prob_artist0 = D(artist_paintings)          # D try to increase this prob
+    prob_artist1 = D(G_paintings)               # D try to reduce this prob
+
+
+    #计算损失
+
+    import time
+
+
+    #训练判断网络的判断能力
+    d_loss_real =criterion(prob_artist0, torch.ones(64, 1)) #设置true为1
+    d_loss_fake =criterion(prob_artist1, torch.zeros(64, 1)) #设置false为0
+    D_loss =d_loss_fake + d_loss_real
+
+    #增加生成网络的伪造能力
+    G_loss = criterion(prob_artist1,torch.ones(64, 1) )
+
+
+
+    # time.sleep(10)
+
+
+    if step %2==1:
+        opt_D.zero_grad()
+        D_loss.backward(retain_graph=True)      # reusing computational graph
+        opt_D.step()
+
+        
+    if step %2==0:
+        
+        opt_G.zero_grad()
+        G_loss.backward()
+        opt_G.step()
+
+    if step % 50 == 0:  # plotting
+        plt.cla()
+        plt.plot(PAINT_POINTS[0], G_paintings.data.numpy()[0], c='#4AD631', lw=3, label='Generated painting',)
+        plt.plot(PAINT_POINTS[0], 2 * np.power(PAINT_POINTS[0], 2) + 1, c='#74BCFF', lw=3, label='upper bound')
+        plt.plot(PAINT_POINTS[0], 1 * np.power(PAINT_POINTS[0], 2) + 0, c='#FF9359', lw=3, label='lower bound')
+        plt.text(-.5, 2.3, 'D accuracy=%.2f (0.5 for D to converge)' % prob_artist0.data.numpy().mean(), fontdict={'size': 13})
+        plt.text(-.5, 2, 'D score= %.2f (-1.38 for G to converge)' % -D_loss.data.numpy(), fontdict={'size': 13})
+        plt.ylim((0, 3));plt.legend(loc='upper right', fontsize=10);plt.draw();plt.pause(0.01)
+
+plt.ioff()
+plt.show()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+
+    # time.sleep(10)
+
+
+    # D_loss = - torch.mean(torch.log(prob_artist0) + torch.log(1. - prob_artist1))
+
+    # G_loss = torch.mean(torch.log(1. - prob_artist1))
